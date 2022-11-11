936120050b6bddd1ee2f84060a46b11f0fea993d
Tom Olschewski <tom.olli@icloud.com>
2022-11-11 20:17:58 +0100

    Aufgabe_03 - Quersummen

---
+++ Info1Übungen/Übung_02/Aufgabe_03 -  Einfache und iterierte Quersumme/quersumme.java
@@ -0,0 +1,32 @@
+public class quersumme {
+  public static void main(String[] args) {
+    
+    // n ist Zahl dessen Quersumme wir bilden und k gibt an ob die normale oder iterierte
+    int n = Integer.parseInt(args[0]);
+    int k  = Integer.parseInt(args[1]);
+
+    //aufruf der iterierten Quersumme, mit der Rekursiven Funtkion quersumme und der Abbruchsbedinung, dass n<10 sein muss
+    if(k!= 0){
+      while(n>9) {
+        n = quersumme(n);
+        if (n>9)
+          System.out.println("Fertige zwischen Summe : " + n);
+      }
+      System.out.println("Iterierte Quersumme : " + n);
+    }
+    
+    //einfacher Aufruf der rekursiven Quersummen Funktion für die normale Quersumme
+    else
+      System.out.println("Quersumme =  "  + quersumme(n)); 
+  }
+
+  //Rekursive Funktion, die sich selber so oft aufruft bis n<=9 und dann die Quersumme aufeinander addiert
+  public static int quersumme(int n)  {
+    if (n <= 9) {
+    return n;
+    }
+    int temp = quersumme(n/10);
+    System.out.println("Temporäre Zwischenszmme : " + temp);
+    return n%10 + temp;
+  }
+}
