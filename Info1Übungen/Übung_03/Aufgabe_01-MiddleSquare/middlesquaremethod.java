9d4332ae0b28845b2b9b440c1f307c6dcb36d23f
Tom Olschewski <tom.olli@icloud.com>
2022-11-17 17:44:29 +0100

    Aufgabe_01 - MiddleSquare hinzugefügt

---
+++ Info1Übungen/Übung_03/Aufgabe_01-MiddleSquare/middlesquaremethod.java
@@ -0,0 +1,39 @@
+class middlesquaremethod {
+  public static void main(String args[]) {
+
+    int n = 0;
+    while(true) {
+      StdOut.println("Please enter your starting number : ");
+      n = StdIn.readInt();
+
+      if (n<0 || n>99){
+        StdOut.println("Your input was invalid, try again.");
+        continue;
+        }
+
+      StdOut.println("Your starting Number is : " + n);
+      break;
+    }
+
+    for (int i = 0; i < 102; i++) {
+      n = n * n;
+
+      if (n > 1000){
+        n = n % 1000;
+        n = n / 10;
+        StdOut.println(n);
+      }
+      
+      else if (n > 100 && n < 1000) {
+        n = n / 10;
+        StdOut.println(n);
+      }
+
+      else if (n < 100) {
+        n = n / 10;
+        StdOut.println(n);
+      }
+
+    }
+  }
+}
